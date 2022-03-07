c30cf5347302eb0649de8700ec3507c65a2469a6
Hossein samiei <hossein@Hosseins-MacBook-Pro.local>
2022-03-07 17:42:11 +0330

    registration file updated !

--- registration.py
+++ registration.py
@@ -1,9 +1,30 @@
-
-
-
-userRedux = '^[0-9]+' 
+import re
+passRedux = '^[0-9]+$' 
 def check_registration_rules(**kwargs):
-    for username , password in kwargs.items() :
-        if len(username) > 3 and len(password) > 5 and username != "codecup" and username != "quera" :
-            pass
+    validateList = []
+    for username , password in kwargs.items() : 
 
+        if len(username) > 3 and len(password) > 5 :
+            if(username != "codecup" and username != "quera") :
+                if  not(re.search(passRedux ,password)):
+                    validateList.append(username)
+                    
+    return validateList
+
+
+
+
+
+                
+
+dict = {'alii': '1123313' , 'hossein' : '234#erkjf3' , 'mmohadese' : '09238833' , 'yekta' : '892e247'}
+print(check_registration_rules(**dict))
+
+
+
+  
+
+
+
+
+    
